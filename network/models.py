2a45b05bf85d5505092a4402e1b0e4c138b06988
GretaRob <greta.robichaud@gmail.com>
2021-05-29 19:05:22 +0700

    created post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    content = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='authors')
+    likes = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            'content': self.content,
+            'date_posted': self.strftime("%b %d %Y, %I:%M %p"),
+            'author': self.author,
+            'likes': self.likes
+        }
